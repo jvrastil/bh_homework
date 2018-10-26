aead6f7f2d6b9c969da41acf63dafbc0984648df
Jan Vraštil <jan.vrastil@firma.seznam.cz>
2018-10-26 13:34:29 +0200

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bh-homework'`, () => {
+  it(`should have as title 'bohemia-interactive-homework'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bh-homework');
+    expect(app.title).toEqual('bohemia-interactive-homework');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bh-homework!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bohemia-interactive-homework!');
   });
 });
